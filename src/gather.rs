a78435827f0ed3fedc394c89130494b7959b1c7b
Blaise Baptist <blaise.baptist@gmail.com>
2025-06-16 22:00:09 +0000

    lot of things

--- src/gather.rs
+++ src/gather.rs
@@ -1,30 +1,153 @@
 pub mod feed {
-    use rscam::{Camera, Config, CtrlData, Frame};
-    use std::sync::mpsc::{channel, Receiver, Sender};
-    pub struct Cameras {
-        sender: Sender<Vec<Vec<u8>>>,
-        pub receiver: Option<Receiver<Vec<Vec<u8>>>>, //vec differences
-        cameras: Vec<Receiver<Frame>>,
-    }
-    impl Cameras {
-        fn new(cameras: Vec<Camera>) -> Self {
-            let (sender, receiver) = channel::<Vec<Vec<u8>>>();
-            let async_cameras = cameras.into_iter().map(|v| into_async(v)).collect();
-            Cameras {
-                sender,
-                receiver: Some(receiver),
-                cameras: async_cameras,
+    use rscam::{Camera, Config, Frame};
+    use std::collections::HashMap;
+    use std::sync::mpsc::{channel, Sender};
+    use std::thread;
+    use std::time::{Duration, Instant};
+    pub type ImageDiff = Vec<u8>;
+    type CFrame = (Frame, usize);
+    pub const FRAME_RATE: u32 = 30;
+    pub const RESOLUTION: (u32, u32) = (1920, 1080);
+    pub const FORMAT: [u8; 4] = *b"RGB3";
+    const CONFIG: Config = Config {
+        interval: (1, FRAME_RATE),
+        resolution: RESOLUTION,
+        format: &FORMAT,
+        field: rscam::FIELD_NONE,
+        nbuffers: 32,
+    };
+    pub fn start_cameras(cameras: Vec<&str>, sender: Sender<Vec<ImageDiff>>) {
+        let (c_frame_send, c_frame_receive) = channel::<CFrame>();
+        let num_cameras = cameras.len();
+        cameras
+            .into_iter()
+            .enumerate()
+            .for_each(|v| start_camera(v.0, v.1.to_string(), c_frame_send.clone()));
+        let mut old_frames: HashMap<usize, Frame> = HashMap::new();
+        loop {
+            let now = Instant::now();
+            let status = sender.send(
+                (0..num_cameras)
+                    .filter_map(|_| match c_frame_receive.try_recv() {
+                        Ok(new_c_frame) => get_diff(&mut old_frames, new_c_frame).ok(),
+
+                        Err(_) => None,
+                    })
+                    .collect(),
+            );
+            match status {
+                Ok(_) => {}
+                Err(_) => return,
             }
+            let processing_time = now.elapsed();
+            thread::sleep((Duration::from_secs(1) / FRAME_RATE).saturating_sub(processing_time))
         }
     }
-    fn into_async(camera: Camera) -> Receiver<Frame> {
-        let (sender, receiver) = channel::<Frame>();
-        std::thread::spawn(move || loop {
-            match sender.send(camera.capture().unwrap()) {
-                Err(_) => break,
-                Ok(_) => {}
+    fn get_diff(
+        old_frames: &mut HashMap<usize, Frame>,
+        new_c_frame: CFrame,
+    ) -> Result<ImageDiff, String> {
+        let key = new_c_frame.1.clone();
+        return match old_frames.insert(new_c_frame.1, blur(new_c_frame.0)) {
+            Some(old_frame) => Ok(compare_frames(
+                old_frames.get(&key).ok_or("No frame")?,
+                &old_frame,
+            )),
+
+            None => Err("New Camera, No first frame".to_string()),
+        };
+    }
+    fn blur(to_blur: Frame) -> Frame {
+        //     te6b39 mut image use=
+        //         image::ImageBuffer::from_vec(RESOLUTION.0, RESOLUTION.1, to_blur.iter().collect());
+        to_blur
+    }
+    fn start_camera(id: usize, path: String, sender: Sender<CFrame>) {
+        thread::spawn(move || {
+            let mut camera = Camera::new(&path).unwrap();
+
+            //Backlight Compensation
+            camera.set_control(9963804, &3).unwrap();
+
+            //White Balance, Automatic
+            camera.set_control(9963788, &true).unwrap();
+
+            //Auto Exposure
+            camera.set_control(10094849, &3).unwrap();
+
+            //Exposure Time, Absolute
+            // camera.set_control(10094850, &380).unwrap();
+
+            //Focus, Automatic Continuous
+            camera.set_control(10094860, &true).unwrap();
+
+            //Focus, Absolute
+            // camera.set_control(10094858, &1).unwrap();
+
+            print_cam_options(&camera);
+            camera.start(&CONFIG).unwrap();
+
+            loop {
+                match sender.send((camera.capture().unwrap(), id)) {
+                    Ok(_) => {}
+                    Err(v) => {
+                        println!("{:?}", v);
+                        break;
+                    }
+                }
             }
         });
-        receiver
+    }
+    fn compare(v1: [u8; 3], v2: [u8; 3]) -> u8 {
+        value_scale(
+            v1.iter()
+                .zip(v2.iter())
+                .fold(0.0, |acc: f32, (&v1, &v2)| acc + (v1 as f32 - v2 as f32)),
+        )
+    }
+    fn value_scale(v: f32) -> u8 {
+        // let new = if v.abs() > 100.0 { 255 } else { 0 };
+        // return new;
+        return (v / 3.0) as u8;
+    }
+    fn compare_frames(frame1: &Frame, frame2: &Frame) -> ImageDiff {
+        let return_val = frame1
+            .chunks(3)
+            .zip(frame2.chunks(3))
+            .map(|v| compare((*v.0).try_into().unwrap(), (*v.1).try_into().unwrap()))
+            .collect::<ImageDiff>();
+        let num_rays = return_val.iter().fold(0.0, |acc, &e| acc + e as f32) / 255.0;
+        println!("num rays: {}", num_rays);
+        return_val
+    }
+    fn print_cam_options(camera: &rscam::Camera) {
+        let formats: Vec<_> = camera.formats().collect();
+
+        let reses = match camera.resolutions(FORMAT).unwrap() {
+            rscam::ResolutionInfo::Discretes(v) => v,
+            _ => todo!(),
+        };
+        let _: Vec<_> = camera
+            .controls()
+            .inspect(|val| {
+                println!(
+                    "{} {}:{:?}",
+                    val.as_ref().unwrap().id,
+                    val.as_ref().unwrap().name,
+                    val.as_ref().unwrap().data,
+                )
+            })
+            .collect();
+        let intervals: Vec<_> = reses
+            .into_iter()
+            .map(|res| match camera.intervals(FORMAT, res).unwrap() {
+                rscam::IntervalInfo::Discretes(v) => (res, v[0].1),
+                _ => todo!(),
+            })
+            .collect();
+        println!("{:?}", formats);
+        for interval in intervals {
+            println!("res: {:?}, fps: {}", interval.0, interval.1);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,59 @@
-use rscam::{Camera, Config, CtrlData, Frame};
-#[allow(dead_code)]
-fn print_cam_options(camera: &rscam::Camera) {
-    let format = b"RGB3";
-    // let formats: Vec<_> = camera.formats().collect();
-    let _: Vec<_> = camera
-        .controls()
-        .inspect(|val| {
-            println!(
-                "id: {}, name: {}, flags: {}",
-                val.as_ref().unwrap().id,
-                val.as_ref().unwrap().name,
-                val.as_ref().unwrap().flags
-            )
-        })
-        .collect();
-
-    match camera.get_control(9963788).unwrap().data {
-        CtrlData::Boolean {
-            value: v,
-            default: d,
-        } => println!("{},{}", v, d),
-
-        _ => panic!(),
-    }
-    let reses = match camera.resolutions(format).unwrap() {
-        rscam::ResolutionInfo::Discretes(v) => v,
-        _ => todo!(),
-    };
-    let intervals: Vec<_> = reses
-        .into_iter()
-        .map(|res| match camera.intervals(format, res).unwrap() {
-            rscam::IntervalInfo::Discretes(v) => (res, v[0].1),
-            _ => todo!(),
-        })
-        .collect();
-    println!("{}", String::from_utf8(format.clone().to_vec()).unwrap());
-    for interval in intervals {
-        println!("res: {:?}, fps: {}", interval.0, interval.1);
-    }
-}
-fn compare(v1: [u8; 3], v2: [u8; 3]) -> [u8; 3] {
-    [(v1.iter().fold(0, |acc: usize, &e| acc + e as usize) as f32 / 3.0
-        - v2.iter().fold(0, |acc: usize, &e| acc + e as usize) as f32 / 3.0)
-        .round() as u8; 3]
-}
-fn compare_frames(frame1: &Frame, frame2: &Frame) -> Vec<u8> {
-    frame1
-        .chunks(3)
-        .zip(frame2.chunks(3))
-        .flat_map(|v| compare((*v.0).try_into().unwrap(), (*v.1).try_into().unwrap()))
-        .collect()
-}
+mod gather;
+use gather::feed;
+use std::process::Command;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
+// mod imageprocessing;
 fn main() {
-    let mut camera = Camera::new("/dev/video0").unwrap();
-    #[cfg(debug_assertions)]
-    print_cam_options(&camera);
-    let res = (1920, 1080);
-    camera
-        .start(&Config {
-            interval: (1, 60),
-            resolution: res,
-            format: b"RGB3",
-            ..Default::default()
-        })
-        .unwrap();
-
-    let mut oldframe = camera.capture().unwrap();
-
-    for i in 0..1800 {
-        let newframe = camera.capture().unwrap();
-
-        let diff = compare_frames(&oldframe, &newframe); // Vec<u8>
-
-        image::save_buffer_with_format(
-            &format!("images/diff_{:05}.png", i),
-            &diff,
-            res.0,
-            res.1,
-            image::ColorType::Rgb8,
-            image::ImageFormat::Png,
-        )
-        .unwrap();
-        println!("{:05}", i);
-        oldframe = newframe;
+    let path1 = "/dev/video0";
+    let (sender, cameras) = channel::<Vec<feed::ImageDiff>>();
+    thread::spawn(move || feed::start_cameras(vec![path1], sender));
+    let start = Instant::now();
+    let mut frame_count = 0;
+    // let max_frames = feed::FRAME_RATE * 5;
+    let max_frames = 300;
+    while frame_count < max_frames {
+        let diffs = cameras.recv().unwrap();
+        frame_count += diffs.len() as u32;
+        diffs.iter().for_each(|diff| {
+            image::save_buffer_with_format(
+                &format!("../images/diff_{:05}.png", frame_count),
+                &diff,
+                gather::feed::RESOLUTION.0,
+                gather::feed::RESOLUTION.1,
+                image::ColorType::L8,
+                image::ImageFormat::Png,
+            )
+            .unwrap();
+        });
+        // println!("{:05}", i);
     }
+    let framerate = frame_count as f64 / start.elapsed().as_secs_f64();
+    println!(
+        "took {:?} to capture {} frames at {} fps",
+        start.elapsed(),
+        frame_count,
+        framerate
+    );
+    Command::new("ffmpeg")
+        .args([
+            "-framerate",
+            &framerate.to_string(),
+            "-i",
+            "images/diff_%5d.png",
+            "-c:v",
+            "libx264",
+            "-pix_fmt",
+            "yuv420p",
+            &format!(
+                "outputs/output{}.mp4",
+                SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs()
+            ),
+        ])
+        .output()
+        .unwrap();
 }
